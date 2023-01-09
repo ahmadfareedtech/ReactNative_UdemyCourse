7ced4d351b323fb564a2baa2dde60f908325b0b4
ahmadfareedtech <ahmadfareed.work@gmail.com>
2023-01-09 18:31:36 +0500

    expense app in progress

---
+++ 05_expenseTracker/App.js
@@ -0,0 +1,64 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import RecentExpenses from './screens/RecentExpenses'
+import ManageExpense from './screens/ManageExpense';
+import AllExpenses from './screens/AllExpenses';
+import { GlobalStyles } from './constants/styles';
+import { Ionicons } from '@expo/vector-icons'
+
+const Stack = createNativeStackNavigator()
+const BottomTabs = createBottomTabNavigator()
+
+function ExpenseOverview() {
+  return (
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500},
+        headerTintColor: 'white',
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500},
+        tabBarActiveTintColor: GlobalStyles.colors.accent500
+      }}
+    >
+      <BottomTabs.Screen 
+        name='RecentExpenses' 
+        component={RecentExpenses} 
+        options={{
+          title: 'Recent Expenses',
+          tabBarIcon: ({color, size}) => <Ionicons name='hourglass' size={size} color={color} />
+        }}
+      />
+      <BottomTabs.Screen 
+        name='AllExpenses' 
+        component={AllExpenses} 
+        options={{
+          title: 'All Expenses',
+          tabBarIcon: ({color, size}) => <Ionicons name='calendar' size={size} color={color} />
+        }}
+      />
+    </BottomTabs.Navigator>
+  )
+}
+
+export default function App() {
+  return (
+   
+      <>
+        <StatusBar style="light" />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name='ExpensesOverview' 
+              component={ExpenseOverview} 
+              options={{headerShown: false}}
+            />
+            <Stack.Screen 
+              name='ManageExpense' 
+              component={ManageExpense} 
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </>
+  );
+}
---
+++ 05_expenseTracker/screens/ManageExpense.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+
+function ManageExpense() {
+    return (
+        <Text>ManageExpenses Screen</Text>
+    )
+}
+
+export default ManageExpense
